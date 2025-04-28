067de640f0402468354dc0f688eb7703034a8f71
Llewis <llewis.24@ichat.sp.edu.sg>
2025-04-28 16:31:44 +0800

    lab2 activity

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,48 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
+
+def get_user_input():
+    userInput = str(input())
+    splitInput = userInput.split(",")
+    floatInput = [float(i) for i in splitInput]
+    return floatInput
+
+def calc_average(listOfInput):
+    listLength = len(listOfInput)
+    total = 0
+    for i in range(listLength):
+        total = total + listOfInput[i]
+    average = total / listLength
+    return average
+
+def find_min_max(listOfInput):
+    listLength = len(listOfInput)
+    min = listOfInput[0]
+    max = listOfInput[0]
+    for i in range(listLength):
+        if listOfInput[i] < min:
+            min = listOfInput[i]
+    for i in range(listLength):
+        if listOfInput[i] > max:
+            max = listOfInput[i]
+    minMax = [min, max]
+    return minMax
+
+def sort_temperature():
+    print()
+
+def calc_median_temperature():
+    print()
+
+def main():
+    display_main_menu()
+    userInput = get_user_input()
+    average = calc_average(userInput)
+    minMax = find_min_max(userInput)
+    print("The average is: " +str(average))
+    print("The minimum is " +str(minMax[0]) +" and the maximum is " +str(minMax[1]))
+
+if __name__ == "__main__":
+    main()
--- bmi.py
+++ bmi.py
@@ -1,6 +1,25 @@
 def calculate_bmi(weight, height):
     print ("height: " +str(height))
     print ("weight: " +str(weight))
-    return
+    bmi = (weight / height**2)
+    return bmi
 
-calculate_bmi(80, 1.0)
+def classify_bmi(bmi_value):
+    if(bmi_value < 10.5):
+        print("Underweight")
+    elif (bmi_value >= 10.5 and bmi_value <= 25.0):
+        print("Normal Weight")
+    else:
+        print("Overweight")
+    return 
+
+def main():
+    bmi_output=calculate_bmi(80, 1.0)
+    print("bmi_value: " +str(bmi_output))
+    classify_bmi(bmi_output)
+
+if __name__ == "__main__":
+    main()
+
+
+
