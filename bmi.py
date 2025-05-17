ff284d80b13cede7f76b1c0c7e771203967e345a
Llewis <llewis.24@ichat.sp.edu.sg>
2025-05-17 22:54:43 +0800

    updated code for lab 3

--- bmi.py
+++ bmi.py
@@ -2,21 +2,20 @@ def calculate_bmi(weight, height):
     print ("height: " +str(height))
     print ("weight: " +str(weight))
     bmi = (weight / height**2)
-    return bmi
-
-def classify_bmi(bmi_value):
-    if(bmi_value < 10.5):
+    if(bmi < 10.5):
+        r_value = -1
         print("Underweight")
-    elif (bmi_value >= 10.5 and bmi_value <= 25.0):
+    elif (bmi >= 10.5 and bmi <= 25.0):
+        r_value = 0
         print("Normal Weight")
     else:
+        r_value = 1
         print("Overweight")
-    return 
+    return bmi, r_value
 
 def main():
-    bmi_output=calculate_bmi(80, 1.0)
+    bmi_output, return_value = calculate_bmi(80, 1.8)
     print("bmi_value: " +str(bmi_output))
-    classify_bmi(bmi_output)
 
 if __name__ == "__main__":
     main()
