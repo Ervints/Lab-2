3ed381823940f4fa6bbdb325f72ce39e53e33911
Llewis <llewis.24@ichat.sp.edu.sg>
2025-05-18 17:44:06 +0800

    changed for exercise2 of lab3

--- bmi.py
+++ bmi.py
@@ -4,20 +4,17 @@ def calculate_bmi(weight, height):
     bmi = (weight / height**2)
     if(bmi < 10.5):
         r_value = -1
-        print("Underweight")
     elif (bmi >= 10.5 and bmi <= 25.0):
         r_value = 0
-        print("Normal Weight")
     else:
         r_value = 1
-        print("Overweight")
     return bmi, r_value
 
 def main():
-    bmi_output, return_value = calculate_bmi(80, 1.8)
+    bmi_output, return_value = calculate_bmi(80, 1.9)
     print("bmi_value: " +str(bmi_output))
 
-if __name__ == "__main__":
+if __name__ == "__main__":   
     main()
 
 
